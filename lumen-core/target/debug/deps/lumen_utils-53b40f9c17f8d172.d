447573169650608689c640c23b09e8d7f8554d47
Tomás Duarte <tomasrsduarte@gmail.com>
2021-04-18 23:06:40 +0100

    Add gitignore

--- lumen-core/target/debug/deps/lumen_utils-53b40f9c17f8d172.d
+++
@@ -1,6 +0,0 @@
-D:\Projects\Rust\lumen\lumen-utils\target\debug\deps\lumen_utils-53b40f9c17f8d172.exe: src\lib.rs src\color.rs
-
-D:\Projects\Rust\lumen\lumen-utils\target\debug\deps\lumen_utils-53b40f9c17f8d172.d: src\lib.rs src\color.rs
-
-src\lib.rs:
-src\color.rs:
